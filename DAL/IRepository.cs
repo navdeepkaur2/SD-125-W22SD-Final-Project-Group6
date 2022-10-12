46e07c1b4168aa8bf75f759ac151d43469229663
20Parrots <navdeepkaurgumti9888@gmail.com>
2022-10-12 11:09:29 -0500

    add IRepository

---
+++ DAL/IRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace SD_340_W22SD_Final_Project_Group6.DAL; 
+
+public interface IRepository<T>  {
+    void Create(T entity);
+
+    T Find(Func<T, bool> predicate);
+
+    ICollection<T> GetAll();
+
+    ICollection<T> GetList(Func<T, bool> predicate);
+
+    T Update(T entity);
+
+    void Delete(T entity);
+
+    void Save();
+}
