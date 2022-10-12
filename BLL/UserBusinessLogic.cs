353d76c842de80928881541ec4b8324e07254b6b
Jaesung Hong <jezsung@gmail.com>
2022-10-12 17:32:29 -0500

    add UserBusinessLogic

---
+++ BLL/UserBusinessLogic.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SD_340_W22SD_Final_Project_Group6.Data;
+using SD_340_W22SD_Final_Project_Group6.Models;
+
+namespace SD_340_W22SD_Final_Project_Group6.BLL
+{
+    public class UserBusinessLogic
+    {
+        public ApplicationDbContext _context;
+        public UserManager<ApplicationUser> _userManager;
+
+        public UserBusinessLogic(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+    }
+}
