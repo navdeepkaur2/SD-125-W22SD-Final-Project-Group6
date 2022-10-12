fc84215947b9bb8eaea8832c811f460bf92cd5fd
Jaesung Hong <jezsung@gmail.com>
2022-10-12 13:01:51 -0500

    rename Find to Get

--- DAL/IRepository.cs
+++ DAL/IRepository.cs
@@ -1,15 +1,16 @@
-﻿namespace SD_340_W22SD_Final_Project_Group6.DAL; 
+﻿namespace SD_340_W22SD_Final_Project_Group6.DAL;
 
-public interface IRepository<T>  {
+public interface IRepository<T>
+{
     void Create(T entity);
 
-    T Find(int id);
+    T Get(int id);
 
-    T Find(Func<T, bool> predicate);
-
-    ICollection<T> GetAll();
+    T Get(Func<T, bool> predicate);
 
     ICollection<T> GetList(Func<T, bool> predicate);
+
+    ICollection<T> GetAll();
 
     T Update(T entity);
 
