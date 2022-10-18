112569787f2a47e00ba3a50a0e5b3868b8104145
Jaesung Hong <jezsung@gmail.com>
2022-10-18 14:35:11 -0500

    add unit testing

---
+++ SD-125-W22SD-Final-Project-UnitTest-Group6/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SD_125_W22SD_Final_Project_UnitTest_Group6
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
