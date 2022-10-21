7927d888b444c6dc09a5a1256c1a6073b412cd98
Jaesung Hong <jezsung@gmail.com>
2022-10-21 00:40:06 -0500

    add ShouldCreateTicket to TicketsBusinessLogicTest

--- SD-125-W22SD-Final-Project-Group6/DAL/CommentsRepository.cs
+++ SD-125-W22SD-Final-Project-Group6/DAL/CommentsRepository.cs
@@ -7,47 +7,49 @@ namespace SD_340_W22SD_Final_Project_Group6.DAL
     {
         private readonly ApplicationDbContext _context;
 
+        public CommentsRepository() { }
+
         public CommentsRepository(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        public void Create(Comment entity)
+        public virtual void Create(Comment entity)
         {
             _context.Add(entity);
         }
 
-        public Comment? Find(Func<Comment, bool> predicate)
+        public virtual Comment? Find(Func<Comment, bool> predicate)
         {
             return _context.Comments.FirstOrDefault(predicate);
         }
 
-        public Comment? FindById(int id)
+        public virtual Comment? FindById(int id)
         {
             return _context.Comments.FirstOrDefault(c => c.Id == id);
         }
 
-        public ICollection<Comment> FindList(Func<Comment, bool> predicate)
+        public virtual ICollection<Comment> FindList(Func<Comment, bool> predicate)
         {
             return _context.Comments.Where(predicate).ToList();
         }
 
-        public ICollection<Comment> GetAll()
+        public virtual ICollection<Comment> GetAll()
         {
             return _context.Comments.ToList();
         }
 
-        public Comment Update(Comment entity)
+        public virtual Comment Update(Comment entity)
         {
             return _context.Comments.Update(entity).Entity;
         }
 
-        public void Delete(Comment entity)
+        public virtual void Delete(Comment entity)
         {
             _context.Remove(entity);
         }
 
-        public void Save()
+        public virtual void Save()
         {
             _context.SaveChanges();
         }
---
+++ SD-125-W22SD-Final-Project-UnitTest-Group6/TicketsBusinessLogicTest.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using SD_340_W22SD_Final_Project_Group6.BLL;
+using SD_340_W22SD_Final_Project_Group6.DAL;
+using SD_340_W22SD_Final_Project_Group6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SD_125_W22SD_Final_Project_UnitTest_Group6
+{
+    [TestClass]
+    public class TicketsBusinessLogicTest
+    {
+        [TestMethod]
+        public async Task ShouldCreateTicket()
+        {
+            // Arrange
+            var testUser = new ApplicationUser
+            {
+                Id = "UserId1"
+            };
+            var testProject = new Project
+            {
+                Id = 1,
+            };
+            var testTicket = new Ticket
+            {
+                Id = 1,
+            };
+
+            var mockUserManager = new Mock<FakeUserManager>();
+            var mockProjectsRepository = new Mock<ProjectsRepository>();
+            var mockTicketsRepository = new Mock<TicketsRepository>();
+            var mockCommentsRepository = new Mock<CommentsRepository>();
+
+            mockUserManager
+                .Setup(x => x.FindByIdAsync(testUser.Id))
+                .ReturnsAsync(testUser);
+            mockProjectsRepository
+                .Setup(x => x.FindById(testProject.Id))
+                .Returns(testProject);
+
+            var ticketsBusinessLogic = new TicketsBusinessLogic(mockUserManager.Object, mockProjectsRepository.Object, mockTicketsRepository.Object, mockCommentsRepository.Object);
+
+            // Act
+            await ticketsBusinessLogic.Create(testTicket, testProject.Id, testUser.Id);
+
+            // Assert
+            Assert.AreEqual(testProject.Id, testTicket.Project?.Id);
+            Assert.AreEqual(testUser.Id, testTicket.Owner?.Id);
+            mockTicketsRepository.Verify(x => x.Create(testTicket), Times.Once);
+            mockTicketsRepository.Verify(x => x.Save(), Times.Once);
+        }
+    }
+}
