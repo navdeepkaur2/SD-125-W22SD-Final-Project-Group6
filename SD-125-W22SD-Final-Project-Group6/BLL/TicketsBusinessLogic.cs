c97f93ab575f2fcf86ff379a21b79e7802c2c1db
Jaesung Hong <jezsung@gmail.com>
2022-10-21 09:48:25 -0500

    fix unnecessary async

--- SD-125-W22SD-Final-Project-Group6/BLL/TicketsBusinessLogic.cs
+++ SD-125-W22SD-Final-Project-Group6/BLL/TicketsBusinessLogic.cs
@@ -46,7 +46,7 @@ namespace SD_340_W22SD_Final_Project_Group6.BLL
             return _ticketsRepository.GetAll().ToList();
         }
 
-        public async Task Update(Ticket ticket)
+        public void Update(Ticket ticket)
         {
             _ticketsRepository.Update(ticket);
             _ticketsRepository.Save();
